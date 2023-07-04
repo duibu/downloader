a36bc579a12aee1254ccae0186001034fad3535a
dong.w <wdong@yozosoft.com>
2023-07-04 18:53:45 +0800

    feat: input  validate

--- lib/validate/inquirervalidate.py
+++ lib/validate/inquirervalidate.py
@@ -6,3 +6,8 @@ def validate_number(_, value):
         return True
     except ValueError:
         return False
+
+def validate_input(value):
+    if not value:
+        raise inquirer.errors.ValidationError('', reason='该项为必填项')
+    return True
